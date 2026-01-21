818088f891bd121917a8ed32259ed8266b16edfb
root <root@ip-172-31-21-144.ec2.internal>
2026-01-21 05:04:16 +0000

    version 2 update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<h1>Hello World! this is version 2</h1>';
+    return '<h1>Hello World! this is version 3</h1>';
   }
 }
