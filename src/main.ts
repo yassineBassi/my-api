636cbe189d198f01ece2da1165dc669fe3ac615b
BASSI Yassine <yassinebassii@gmail.com>
2026-01-27 02:35:26 +0100

    logs without colors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { LoggingInterceptor } from './interceptor/Logging.interceptor';
 import { AllExceptionsFilter } from './interceptor/all-exception-filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    logger: ['log', 'error', 'warn', 'debug'],
+  });
   app.useGlobalInterceptors(new LoggingInterceptor())
   app.useGlobalFilters(new AllExceptionsFilter())
   await app.listen(process.env.PORT ?? 3000);
