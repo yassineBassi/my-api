1867cb6bacf6911dffb9ff311db2c38f22c17260
BASSI Yassine <yassinebassii@gmail.com>
2026-01-28 23:14:46 +0100

    version 5

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,12 +3,12 @@ import { Injectable, HttpException, HttpStatus, Logger  } from '@nestjs/common';
 export class AppService {
   private readonly logger = new Logger('HTTP');
   getHello(): string {
-    return '<h1>Hello World! this is version 4 !!!!</h1>';
+    return '<h1>Hello World! this is version 5 !!!!</h1>';
   }
 
   healthCheck(): string {
     this.logger.log('test')
     throw new HttpException("error", HttpStatus.INTERNAL_SERVER_ERROR)
-    return '<h1>Hello World! this is version 4 !!!!</h1>';
+    return '<h1>Hello World! this is version 5 health !!!!</h1>';
   }
 }
